74062a5f35e8328ba1c08910d4440a3612f9c943
gonzaloruizmijangos <gonzalo.ruiz.mijangos@outlook.com>
2016-05-09 11:31:38 -0500

    Calculo de  todos los valores...

--- TSP Dashboard/Scripts/WBS.js
+++ TSP Dashboard/Scripts/WBS.js
@@ -1,4 +1,5 @@
-﻿jQuery(document).ready(function () {
+﻿var cIdProcesoGlobal = "";
+jQuery(document).ready(function () {
 
     GenerarRowProcesos(1);
     $("#iIdPorcentajeTareas_Análisis").on('change', function (e) {
@@ -21,6 +22,7 @@ function GenerarRowProcesos(iIdProceso)
         success: function (response) {
             $rowProcesos.html("");
             $rowProcesos.append(response.d);
+       
         }
     });
 }
@@ -28,36 +30,26 @@ function GenerarRowProcesos(iIdProceso)
 function PorcentajeDeTareas(cIdTitle)
 {  
     var rowEstimate = '#iIdEstimate_';
-    var rowTiempoTotalProcesos = $('#iIdTotalProceso_').val();
+    var rowTiempoTotalProcesos = $('#iIdTotalProceso_' + ObtenerProceso(cIdTitle.replace("iIdPorcentajeTareas_", ""))).val();
     var rowPorcentajeTarea = $('#' + cIdTitle).val();
     var dOriginalEstimate = rowTiempoTotalProcesos * (rowPorcentajeTarea / 100);
-        var dOriginalEstimate = rowTiempoTotalProcesos * (rowPorcentajeTarea / 100);
 
     $(rowEstimate + cIdTitle.replace("iIdPorcentajeTareas_", "")).val(dOriginalEstimate);
-    $('#iIdHorasAcumuladas_' + cIdTitle.replace("iIdPorcentajeTareas_", "")).val(HorasAcumuladas(cIdTitle.replace("iIdPorcentajeTareas_", "")));
-    //$.ajax({
-    //    type: "POST",
-    //    url: "../../Class/WBS.asmx/OriginalEstimate",
-    //    async: false,
-    //    data: '{"dTiempoTotalDelProceso":"' + rowTiempoTotalProcesos + '", "dPorcentajeDeTarea":"' + rowPorcentajeTarea + '"}',
-    //    contentType: "application/json; charset=utf-8",
-    //    dataType: "json",
-    //    success: function (response) {
-    //        $(rowEstimate + cIdTitle.replace("iIdPorcentajeTareas_", "")).val(response.d);
-    //        //$('#iIdHorasAcumuladas_' + cIdTitle.replace("iIdPorcentajeTareas_", "")).val(response.d);
-    //    }
-    //});
+    var dHoras =HorasAcumuladas( cIdTitle.replace("iIdPorcentajeTareas_", ""));
+    $('#iIdHorasAcumuladas_' + cIdTitle.replace("iIdPorcentajeTareas_", "")).val(dHoras);
+
 }
 
 function HorasAcumuladas(cIdProceso)
 {
     var cProceso = "";
     var dHorasAcumularas = 0;
+
     $.ajax({
         type: "POST",
         url: "../../Class/WBS.asmx/Proceso",
         async: false,
-        data: '{"cIdProceso":"' + cIdProceso + '"}',
+        data: '{"cIdProceso":"' + cIdProcesoGlobal + '"}',
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (response) {
@@ -65,24 +57,34 @@ function HorasAcumuladas(cIdProceso)
         }
     });
     $(cProceso.split(',')).each(function (index, domEle) {
-        dHorasAcumularas =parseFloat(dHorasAcumularas + $('#iIdEstimate_' + domEle.replace(" ","_")).val());
+        dHorasAcumularas = parseFloat(parseFloat(dHorasAcumularas) + parseFloat($('#iIdEstimate_' + domEle.replace(" ", "_")).val()));
     });
-    
-    //var analisis = $('#iIdEstimate_Análisis').val();
-    //var plan = $('#iIdEstimate_Plan').val();
-    //var Entendi =$('#iIdEstimate_Entendimiento').val();
-    //var elabora =$('#iIdEstimate_Elaborar_Caso_de_Uso').val();
-    //var reviw =$('#iIdEstimate_Review_de_Caso_de_Uso').val();
-    //var plan_pruebas = $('#iIdEstimate_Elaborar_Plan_de_Pruebas').val();
-    //var reviewPlan =$('#iIdEstimate_Review_de_Plan_de_Pruebas').val();
-    //var peer =$('#iIdEstimate_Peer_Review_de_Caso_de_Uso_y_Plan_de_Pruebas').val();
-    //var vali =$('#iIdEstimate_Validación_de_Caso_de_Uso_y_Plan_de_Pruebas').val();
-    //var post = $('#iIdEstimate_Postmortem').val();
-    //dHorasAcumularas = parseFloat(analisis) + parseFloat(plan) + parseFloat(Entendi) + parseFloat(elabora) + parseFloat(reviw) + parseFloat(plan_pruebas) + parseFloat(reviewPlan) + parseFloat(peer) + parseFloat(vali) + parseFloat(post);
-    return dHorasAcumularas;
+  return dHorasAcumularas;
 
 }
-//function OnSuccess(response) {
-//    var $rowProcesos = $('ul.rowProceso');
-//    $rowProcesos.append(response.d);//Datos[0].htmlinput
-//}
+
+function ObtenerProceso(cIdProceso)
+{
+    switch (cIdProceso) {
+        case ("Análisis"):
+            cIdProcesoGlobal = "Análisis";
+            break;
+        case ("Diseño"):
+            cIdProcesoGlobal = "Diseño";
+            break;
+        case ("Desarrollo"):
+            cIdProcesoGlobal = "Desarrollo";
+            break;
+        case ("Seleccionar_Ganador"):
+            cIdProcesoGlobal = "Seleccionar_Ganador";
+            break;
+    }
+    return cIdProcesoGlobal;
+
+}
+
+function ValorGanado()
+{
+
+}
+
