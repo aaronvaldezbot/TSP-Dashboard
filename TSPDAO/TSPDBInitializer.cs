20ac80365da441902329d9773731e04bacee39a0
gonzaloruizmijangos <gonzalo.ruiz.mijangos@outlook.com>
2016-05-09 13:47:08 -0500

    Cambios de commit

---
+++ TSPDAO/TSPDBInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace TSPDAO
+{
+    public class TSPDBInitializer : CreateDatabaseIfNotExists<TSPDBContext>
+    {
+        protected override void Seed(TSPDBContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
